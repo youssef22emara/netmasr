39242e8f4425c179da491f29a4a43b5484f04132
يوسف آل عمارة <youssef22emara@gmail.com>
2026-04-16 21:28:29 +0200

    Delete server directory

--- server/routes/complaintRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const complaintController = require('../controllers/complaintController');
-
-router.post('/', complaintController.submitComplaint);
-router.get('/track/:id', complaintController.trackComplaint);
-router.get('/stats', complaintController.getDashboardStats);
-
-module.exports = router;
